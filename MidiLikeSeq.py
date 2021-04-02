a5ad924013e8f5d72528ffcbeeba82dab1995f81
Erik <glebellier@protonmail.com>
2021-04-02 11:30:45 +0200

    Add get f0 loudness from midi like (need fix)

--- MidiLikeSeq.py
+++ MidiLikeSeq.py
@@ -1,7 +1,10 @@
+import numpy as np
+
 class MidiLikeSeq:
     def __init__(self):
         self.seq = []
         self.notes_on = [] # list of pitches of notes played a this given time
+        self.duration = 0
 
 
     def note_on(self, pitch):
@@ -23,6 +26,7 @@ class MidiLikeSeq:
 
     def time_shift(self, delay):
         self.seq.append("TIME_SHIFT<{}>".format(delay))
+        self.duration += delay
 
     def show(self, indexes = None):
         if indexes == None:
@@ -50,8 +54,6 @@ class MidiLikeSeq:
         return True
 
 
-
-
     def save(self, filename):
         file = open(filename, "w")
         for task in self.seq:
@@ -65,6 +67,45 @@ class MidiLikeSeq:
             self.seq.append(line[:-1])
             line = file.readline()
         file.close()
+
+    def get_f0_loudness(self, frame_rate):
+        """ extract f0 and loudness for monophonic tracks"""
+
+
+        def write_events(current_pitch, current_loudness, note_ON, i_current_time, i_previous_event_time, pitch, loudness):
+                pitch[i_previous_event_time:i_current_time] = current_pitch * note_ON
+                loudness[i_previous_event_time:i_current_time] = current_loudness *note_ON
+                return pitch, loudness
+
+        current_pitch = 0
+        current_loudness = 0
+        i_current_time = 0
+        i_previous_event_time = 0 
+        note_ON = False
+        
+        pitch = np.zeros(int(self.duration * frame_rate))
+        loudness = np.zeros(int(self.duration * frame_rate))
+        
+        for task in self.seq:
+            if task[:7] == "SET_VEL":
+                pitch, loudness = write_events(current_pitch, current_loudness, note_ON, i_current_time, i_previous_event_time, pitch, loudness)
+                current_loudness = int(float(task[13:len(task)-1]))
+
+            if task[:7] == "TIME_SH":
+                time_shift = float(task[11:len(task)-1])
+                i_previous_event_time = i_current_time
+                i_current_time += time_shift // frame_rate
+
+            if task[:7] == "NOTE_ON":
+                pitch, loudness = write_events(current_pitch, current_loudness, note_ON, i_current_time, i_previous_event_time, pitch, loudness)
+                note_ON = True
+                current_pitch = int(float(task[8:len(task)-1]))
+
+            if task[:7] == "NOTE_OF":
+                pitch, loudness = write_events(current_pitch, current_loudness, note_ON, i_current_time, i_previous_event_time, pitch, loudness)
+                note_ON = False
+        
+        return pitch, loudness
 
 
 
