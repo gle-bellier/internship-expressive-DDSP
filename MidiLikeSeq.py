091e9ac657d7f8dfd5e1757a039a77fc89e8eb63
Erik <glebellier@protonmail.com>
2021-04-06 15:09:50 +0200

    Add extraction of f0 and loudness for midilike

--- MidiLikeSeq.py
+++ MidiLikeSeq.py
@@ -1,10 +1,13 @@
 import numpy as np
 
+
 class MidiLikeSeq:
     def __init__(self):
         self.seq = []
         self.notes_on = [] # list of pitches of notes played a this given time
         self.duration = 0
+        self.pitch = None
+        self.loudness = None
 
 
     def note_on(self, pitch):
@@ -68,14 +71,17 @@ class MidiLikeSeq:
             line = file.readline()
         file.close()
 
-    def get_f0_loudness(self, frame_rate):
+    def get_f0_loudness_time(self, frame_rate):
         """ extract f0 and loudness for monophonic tracks"""
 
 
-        def write_events(current_pitch, current_loudness, note_ON, i_current_time, i_previous_event_time, pitch, loudness):
-                pitch[i_previous_event_time:i_current_time] = current_pitch * note_ON
-                loudness[i_previous_event_time:i_current_time] = current_loudness *note_ON
-                return pitch, loudness
+        def write_events(current_pitch, current_loudness, note_ON, i_current_time, i_previous_event_time):
+                #print("Index previous time {}: , Index Current time {}".format(i_previous_event_time, i_current_time))
+                #print("Pitch : ", current_pitch)
+                #print("Loudness: ", current_loudness)
+                #print("Note On : ", note_ON)
+                self.pitch[i_previous_event_time:i_current_time] = current_pitch * note_ON
+                self.loudness[i_previous_event_time:i_current_time] = current_loudness *note_ON
 
         current_pitch = 0
         current_loudness = 0
@@ -83,29 +89,34 @@ class MidiLikeSeq:
         i_previous_event_time = 0 
         note_ON = False
         
-        pitch = np.zeros(int(self.duration * frame_rate))
-        loudness = np.zeros(int(self.duration * frame_rate))
+        self.pitch = np.zeros(int(self.duration * frame_rate))
+        self.loudness = np.zeros(int(self.duration * frame_rate))
+
         
         for task in self.seq:
             if task[:7] == "SET_VEL":
-                pitch, loudness = write_events(current_pitch, current_loudness, note_ON, i_current_time, i_previous_event_time, pitch, loudness)
+                write_events(current_pitch, current_loudness, note_ON, i_current_time, i_previous_event_time)
                 current_loudness = int(float(task[13:len(task)-1]))
 
             if task[:7] == "TIME_SH":
                 time_shift = float(task[11:len(task)-1])
                 i_previous_event_time = i_current_time
-                i_current_time += time_shift // frame_rate
+                i_current_time += int(time_shift // (1/frame_rate))
 
             if task[:7] == "NOTE_ON":
-                pitch, loudness = write_events(current_pitch, current_loudness, note_ON, i_current_time, i_previous_event_time, pitch, loudness)
+                write_events(current_pitch, current_loudness, note_ON, i_current_time, i_previous_event_time)
                 note_ON = True
                 current_pitch = int(float(task[8:len(task)-1]))
+                i_previous_event_time = i_current_time
 
             if task[:7] == "NOTE_OF":
-                pitch, loudness = write_events(current_pitch, current_loudness, note_ON, i_current_time, i_previous_event_time, pitch, loudness)
+                write_events(current_pitch, current_loudness, note_ON, i_current_time, i_previous_event_time)
                 note_ON = False
+                i_previous_event_time = i_current_time
+            
+            t = np.arange(self.pitch.shape[0])/frame_rate
         
-        return pitch, loudness
+        return self.pitch, self.loudness, t
 
 
 
