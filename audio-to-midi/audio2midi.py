97980bf37e56dd38959daa155433b8ec7e48a535
Erik <glebellier@protonmail.com>
2021-04-16 09:21:58 +0200

    New experiments with threshold

--- audio-to-midi/audio2midi.py
+++ audio-to-midi/audio2midi.py
@@ -31,9 +31,7 @@ class Audio2MidiConverter:
             global_min = np.min(loudness)
         
         l = loudness-global_min
-        print("l : ", l)
         L =  127*np.abs(l)/np.abs(global_peak-global_min)
-        print("Loudness : ", L )
         return L 
 
     def compute_dv(self, confidence):
@@ -42,6 +40,12 @@ class Audio2MidiConverter:
         dv = confidence_pad - conf_delay
         return dv[:-1]
 
+    def get_loudness_changes(self, loudness, threshold):
+        dl = self.compute_dv(loudness)
+        dln = dl/np.max(np.abs(dl))
+        positiv = dln > threshold
+        negativ = dln < -threshold
+        return negativ, positiv
 
     def get_confidence_changes(self, confidence, threshold):
         dv = self.compute_dv(confidence)
@@ -70,7 +74,7 @@ class Audio2MidiConverter:
     
 
 
-    def process(self, sampling_rate = 48000, block_size = 480, threshold = 0.15, min_note_length = 0.01):
+    def process(self, sampling_rate = 48000, block_size = 480, threshold_confidence = 0.15, threshold_loudness = 0.20, min_note_length = 0.01,  verbose = False):
         self.sampling_rate = sampling_rate
         self.min_note_length = min_note_length
 
@@ -78,14 +82,22 @@ class Audio2MidiConverter:
 
         ext = Extractor()
         time, frequency, confidence, loudness = ext.get_time_f0_confidence_loudness(filename, sampling_rate, block_size, write=True)
+        loudness = self.dB2midi(loudness)
+
+        neg_conf_changes, pos_conf_changes = self.get_confidence_changes(confidence, threshold_confidence)
+        midi_pitch_changes = self.get_midi_pitch_changes(frequency)
+        neg_loud_changes, pos_loud_changes = self.get_loudness_changes(loudness, threshold_loudness)
+
         
 
-        pos_conf_changes, neg_conf_changes = self.get_confidence_changes(confidence, threshold)
-        midi_pitch_changes = self.get_midi_pitch_changes(frequency)
+        conf_onsets = np.logical_or(pos_conf_changes, neg_conf_changes)
+        loud_onsets = np.logical_or(pos_loud_changes, neg_loud_changes)
+        pitch_onsets = midi_pitch_changes
+
 
         all_onsets = np.logical_or(pos_conf_changes, neg_conf_changes) # May be change to AND
         all_onsets = np.logical_or(all_onsets, midi_pitch_changes) # May be change to OR 
-
+        all_onsets = np.logical_or(all_onsets, loud_onsets)   
 
         notes = []
         current_note = {"on": None, "off": None}
@@ -105,15 +117,92 @@ class Audio2MidiConverter:
                     notes.append(current_note)
                     current_note = {"on": None, "off": None}
 
+
+
+
+        if verbose:
+            span = time.shape[0]//8
+            middle = time.shape[0]//2
+            a, b = middle - span, middle + span
+
+            ax1 = plt.subplot(221)        
+            ax1.plot(time[a:b], frequency[a:b]/np.max(frequency), label = "Normalized f0")
+            ax1.plot(time[a:b], conf_onsets[a:b], label = "Onsets")
+            ax1.legend()
+            ax1.set_title("Conf Onsets")
+
+
+            ax2 = plt.subplot(222)       
+            ax2.plot(time[a:b], frequency[a:b]/np.max(frequency), label = "Normalized f0")
+            ax2.plot(time[a:b], pitch_onsets[a:b], label = "Onsets")
+            ax2.legend()
+            ax2.set_title('Pitch Onsets')
+            
+            ax3 = plt.subplot(223)
+            ax3.plot(time[a:b], frequency[a:b]/np.max(frequency), label = "Normalized f0")
+            ax3.plot(time[a:b], loud_onsets[a:b], label = "Onsets")
+            ax3.legend()
+            ax3.set_title('Loudness Onsets')
+
+            ax3 = plt.subplot(224)
+            ax3.plot(time[a:b], frequency[a:b]/np.max(frequency), label = "Normalized f0")
+            ax3.plot(time[a:b], np.logical_and(loud_onsets[a:b], conf_onsets[a:b]), label = "Onsets")
+            ax3.legend()
+            ax3.set_title('Loudness AND Conf Onsets')
+
+            plt.legend()
+            plt.show()
+
+
+
+
+
+        if verbose:
+            span = time.shape[0]//8
+            middle = time.shape[0]//2
+            a, b = middle - span, middle + span
+
+            ax1 = plt.subplot(212)        
+            ax1.plot(time[a:b], frequency[a:b]/np.max(frequency), label = "Normalized f0")
+            ax1.plot(time[a:b], np.ones((b-a))*threshold_confidence)
+
+            ax1.plot(time[a:b], np.abs(self.compute_dv(confidence[a:b])), label = "Dv Confidence")
+            ax1.set_title("f0 confidence")
+
+            ax2 = plt.subplot(221)
+       
+            ax2.plot(time[a:b], frequency[a:b]/np.max(frequency), label = "Normalized f0")
+            ax2.plot(time[a:b], all_onsets[a:b], label = "Onsets")
+            ax2.set_title('Onsets')
+
+
+            loudness_dv = self.compute_dv(loudness)
+            ax3 = plt.subplot(222)
+            ax3.plot(time[a:b], frequency[a:b]/np.max(frequency), label = "Normalized f0")
+            ax3.plot(time[a:b], np.abs(loudness_dv[a:b])/np.max(loudness_dv), label = "Loudness")
+            ax3.plot(time[a:b], np.ones((b-a))*threshold_loudness)
+            ax3.set_title('Loudness')
+
+
+            plt.legend()
+            plt.show()
+
+
+
+
+
         # remove short notes
 
         notes = [note for note in notes if note["off"] - note["on"] >= min_note_length]
-        loudness = self.dB2midi(loudness)
 
-
+        # add pitch information
         notes_with_pitch = [self.get_note_with_pitch_loudness(note, frequency, loudness) for note in notes]
 
 
+
+
+
+        # writing note in sequence
         print(notes_with_pitch)
         sequence =  music_pb2.NoteSequence()
 
@@ -140,10 +229,11 @@ class Audio2MidiConverter:
 if __name__ == "__main__":
     filename = "violin.wav"
 
-    threshold = 0.10
+    threshold_confidence = 0.05
+    threshold_loudness = 0.15   
     a2m = Audio2MidiConverter(filename)
-    seq_midi = a2m.process(sampling_rate = 48000, block_size = 480, threshold = threshold)
+    seq_midi = a2m.process(sampling_rate = 48000, block_size = 480, threshold_confidence = threshold_confidence, threshold_loudness = threshold_loudness, verbose = True)
 
 
-    note_seq.sequence_proto_to_midi_file(seq_midi, filename[:-4] + "(from-audio)-th{}.mid".format(threshold))
+    note_seq.sequence_proto_to_midi_file(seq_midi, filename[:-4] + "(from-audio)-thC{}-thL{}.mid".format(threshold_confidence, threshold_loudness))
     
