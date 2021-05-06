54f6d8c70e37215ef7586fe5e96def39cf603478
Erik <glebellier@protonmail.com>
2021-05-06 15:22:32 +0200

    Update cleaner methods

--- audio-to-midi/cleaner.py
+++ audio-to-midi/cleaner.py
@@ -12,14 +12,14 @@ class Cleaner:
     def __init__(self):
         pass
 
-    def check(self, midi_file):
+    def check(self, midi_file, epsi_error=0.01):
         mlseq = note_seq.midi_file_to_note_sequence(midi_file)
         previous_note = mlseq.notes[0]
 
         for note in mlseq.notes[1:]:
-            if note.start_time<previous_note.end_time:
-                #print("Note : pitch {}, start {}, end {} ".format(note.pitch, note.start_time, note.end_time))
-                #print("Next note : pitch {}, start {}, end {}".format(previous_note.pitch, previous_note.start_time, previous_note.end_time ))
+            if note.start_time<previous_note.end_time -epsi_error   :
+                print("Note : pitch {}, start {}, end {} ".format(note.pitch, note.start_time, note.end_time))
+                print("Next note : pitch {}, start {}, end {}".format(previous_note.pitch, previous_note.start_time, previous_note.end_time ))
                 return False
             previous_note = note
 
