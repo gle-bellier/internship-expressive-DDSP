b6525a3173d72b0653f3e5efcd1bd6c8e536f7c2
Erik <glebellier@protonmail.com>
2021-05-10 13:40:15 +0200

    Create Dataloader/Dataset

--- create_dataset.py
+++ create_dataset.py
@@ -4,114 +4,117 @@ import torch
 import pandas as pd
 from tqdm import tqdm
 
-import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 import glob
+
+
 from get_contours import ContoursGetter
+from customDataset import ContoursTrainDataset, ContoursTestDataset
+
+sampling_rate = 100
+dataset_path = "dataset-midi-wav/"
+filenames =[file[len(dataset_path):-4] for file in glob.glob(dataset_path + "*.mid")]
 
 
-class ContoursDataset(Dataset):
-    """ Unexpressive and expressive contours Dataset"""
+u_f0 = np.empty(0)
+u_loudness = np.empty(0)
+e_f0 = np.empty(0)
+e_loudness = np.empty(0)
 
 
-    def __init__(self, dataset_path, transform = None):
-
-        self.transform = transform
-
-        self.dataset_path = dataset_path
-        filenames =[file[len(dataset_path):-4] for file in glob.glob(self.dataset_path + "*.mid")]
+list_indexes = [] # store the indexes of ends of tracks
+index_end = 0
 
 
-        self.u_f0 = np.empty(0)
-        self.u_loudness = np.empty(0)
-        self.e_f0 = np.empty(0)
-        self.e_loudness = np.empty(0)
+with tqdm(total=len(filenames)) as pbar:
+    for filename in filenames:
+        midi_file = filename + ".mid"
+        wav_file = filename + ".wav"
+        g = ContoursGetter()
+        u_f0_track, u_loudness_track, e_f0_track, e_loudness_track = g.get_contours(dataset_path, midi_file, wav_file, sampling_rate=16000, block_size=160, max_silence_duration=3, verbose=False)
         
+        u_f0 = np.concatenate((u_f0, u_f0_track))
+        u_loudness = np.concatenate((u_loudness, u_loudness_track))
+        e_f0 = np.concatenate((e_f0, e_f0_track))
+        e_loudness = np.concatenate((e_loudness, e_loudness_track))
 
 
-        with tqdm(total=len(filenames)) as pbar:
-            for filename in filenames:
-                midi_file = filename + ".mid"
-                wav_file = filename + ".wav"
-                g = ContoursGetter()
-                u_f0_track, u_loudness_track, e_f0_track, e_loudness_track = g.get_contours(dataset_path, midi_file, wav_file, sampling_rate=16000, block_size=160, max_silence_duration=3, verbose=False)
-                
-                self.u_f0 = np.concatenate((self.u_f0, u_f0_track))
-                self.u_loudness = np.concatenate((self.u_loudness, u_loudness_track))
-                self.e_f0 = np.concatenate((self.e_f0, e_f0_track))
-                self.e_loudness = np.concatenate((self.e_loudness, e_loudness_track))
+        index_end +=  len(u_f0_track)
+        list_indexes.append(index_end)
 
-                pbar.update(1)
-        
-        self.length = len(self.u_f0)
-        self.segments = []
+        pbar.update(1)
 
 
-
-    def __len__(self):
-        return self.length
-
-
-    def get_random_indexes(self, length = 2000, overlap = 0.0):
-
-        seg_length = int((1 - overlap) * length)
-        i_max = np.floor((self.length - length)/seg_length)
-
-        i = np.random.randint(i_max+1)
-        return i*seg_length, i*seg_length + length
+full_length = len(u_f0)
+# we need to split the dataset between training and testing
 
 
+ratio = 0.7
+i_cut = np.floor(ratio * full_length)
 
+i = 0 
+while i<len(list_indexes) and list_indexes[i]< i_cut:
+    i+=1
+i_cut_real = list_indexes[i]
 
+train_u_f0 = u_f0[:i_cut_real]
+train_u_loudness = u_loudness[:i_cut_real]
+train_e_f0 = u_f0[:i_cut_real]
+train_e_loudness = u_loudness[:i_cut_real]
+train_length = len(train_u_f0)    
 
-
-    def __getitem__(self, idx):
-        
-        start, end = self.get_random_indexes(length=2000, overlap=0.5)
-        #print("Indexes: [{}:{}]".format(start, end))
-        self.segments.append((start,end))
-
-        return self.u_f0[start:end], self.u_loudness[start:end], self.e_f0[start:end], self.e_loudness[start:end]
-
-        
-        
+test_u_f0 = u_f0[i_cut_real:]
+test_u_loudness = u_loudness[i_cut_real:]
+test_e_f0 = u_f0[i_cut_real:]
+test_e_loudness = u_loudness[i_cut_real:]
+test_length = len(test_u_f0)    
 
 
 
 
-if __name__ == "__main__":
-
-    VERBOSE = False
-    dataset_path = "dataset-midi-wav/"
-    g = ContoursDataset(dataset_path)
-    print("Full size : {}".format(g.length/100))
-
-    for i in range(200):
-        u_f0, u_loudness, e_f0, e_loudness = g[0]
-        
-        if VERBOSE:
-            plt.plot(e_f0, label = "wav")
-            plt.plot(u_f0, label = "midi" )
-            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-            plt.title("Frequency comparison")
-            plt.show()
+print("Full size : {}s".format(full_length/sampling_rate))
+print("Training size : {}s".format(train_length/sampling_rate))
+print("Test size : {}s".format(test_length/sampling_rate))
 
 
-            plt.plot(e_loudness, label = "wav")
-            plt.plot(u_loudness, label = "midi" )
-            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-            plt.title("Loudness comparison")
-            plt.show()
 
 
-    if VERBOSE:
-        n = g.length
-        for i in range(len(g.segments)):
-            v = np.zeros(n)
-            a, b = g.segments[i]
-            v[a:b] = 0.5 + i/1000
-            plt.plot(v)
-        plt.title("Segments")
-        plt.show()
+train_dataset = ContoursTrainDataset(train_u_f0, train_u_loudness, train_e_f0, train_e_loudness, sample_length=2000, transform=None)
+test_dataset = ContoursTestDataset(test_u_f0, test_u_loudness, test_e_f0, test_e_loudness, sample_length=2000, transform=None)
+
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=12)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=12)
+
+
+print("Size train set : ", len(train_dataset))
+print("Size test set : ", len(test_dataset))
+
+
+# for train_sample in train_loader:
+#     print(train_sample[0].shape)
+
+# for test_sample in test_loader:
+#     print(test_sample[0].shape)
+
+
+
+
+
+
+
+
+
+
+VERBOSE = True
+
+if VERBOSE:
+    for i in range(len(train_dataset.segments)):
+        v = np.zeros(full_length)
+        a, b = train_dataset.segments[i]
+        v[a:b] = 0.5 + i/1000
+        plt.plot(v)
+    plt.title("Segments")
+    plt.show()
---
+++ customDataset.py
@@ -0,0 +1,75 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+
+
+class ContoursTrainDataset(Dataset):
+    """ Unexpressive and expressive contours Dataset"""
+
+
+    def __init__(self, u_f0, u_loudness, e_f0, e_loudness, sample_length=2000, overlap = 0.3, transform=None):
+
+        self.transform = transform
+        self.sample_length = sample_length
+        self.overlap = overlap
+
+        self.u_f0 = u_f0
+        self.u_loudness = u_loudness
+        self.e_f0 = e_f0
+        self.e_loudness = e_loudness
+
+        self.length = len(self.u_f0)    
+
+
+        self.segments = []
+
+
+
+    def __len__(self):
+        return self.length//self.sample_length
+
+
+    def get_random_indexes(self):
+
+        seg_length = int((1 - self.overlap) * self.sample_length)
+        i_max = np.floor((self.length - self.sample_length)/seg_length)
+
+        i = np.random.randint(i_max+1)
+        return int(i*seg_length), int(i*seg_length + self.sample_length)
+
+
+
+    def __getitem__(self, idx):
+        start, end = self.get_random_indexes()
+        #print("Indexes: [{}:{}]".format(start, end))
+        self.segments.append((start,end))
+        return self.u_f0[start:end], self.u_loudness[start:end], self.e_f0[start:end], self.e_loudness[start:end]
+
+        
+class ContoursTestDataset(Dataset):
+    """ Unexpressive and expressive contours Dataset"""
+
+
+    def __init__(self, u_f0, u_loudness, e_f0, e_loudness, sample_length=2000, transform=None):
+
+        self.transform = transform
+        self.sample_length = sample_length
+
+        self.u_f0 = u_f0
+        self.u_loudness = u_loudness
+        self.e_f0 = e_f0
+        self.e_loudness = e_loudness
+
+        self.length = len(self.u_f0)    
+
+
+    def __len__(self):
+        return self.length//self.sample_length
+
+
+
+    def __getitem__(self, idx):
+        start = int(idx * self.sample_length)
+        end = int(start + self.sample_length)
+        #print("Indexes: [{}:{}]".format(start, end))
+        return self.u_f0[start:end], self.u_loudness[start:end], self.e_f0[start:end], self.e_loudness[start:end]
