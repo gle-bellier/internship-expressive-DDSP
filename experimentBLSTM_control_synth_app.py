5f02f3a3ec4b803a8414016a815d66b3dabadd69
Erik <glebellier@protonmail.com>
2021-05-17 16:02:37 +0200

    Update models and experiments

--- experimentBLSTM_control_synth_app.py
+++ experimentBLSTM_control_synth_app.py
@@ -7,14 +7,15 @@ import glob
 from get_datasets import get_datasets
 from get_contours import ContoursGetter
 from customDataset import ContoursTrainDataset, ContoursTestDataset
-from models.LSTMContours import LSTMContours
+from models.BLSTM_Control_synth_app import BLSTMContours
 
 import torch
 import torch.utils.data
 from torch import nn, optim
 from torch.nn import functional as F
 from torchvision import datasets, transforms
-from torch.autograd import Variable
+from torch.utils.tensorboard import SummaryWriter
+
 
 from sklearn.preprocessing import StandardScaler
 
@@ -24,9 +25,11 @@ if torch.cuda.is_available():
     device = torch.device("cuda:0")
 else:
     device = torch.device("cpu")
-
 print('using', device)
 
+
+
+writer = SummaryWriter("runs/BSLTM_control_synth_app")    
 
 sc = StandardScaler()
 train_loader, test_loader = get_datasets(dataset_file = "dataset/contours.csv", sampling_rate = 100, sample_duration = 20, batch_size = 16, ratio = 0.7, transform=sc.fit_transform)
@@ -36,18 +39,18 @@ train_loader, test_loader = get_datasets(dataset_file = "dataset/contours.csv", 
 ### MODEL INSTANCIATION ###
 
 
-num_epochs = 300
+num_epochs = 10
 learning_rate = 0.01
 input_size = 32
 hidden_size = 64
 num_layers = 2
 
 
-model = LSTMContours(input_size, hidden_size, num_layers).to(device)
+model = BLSTMContours(input_size, hidden_size, num_layers).to(device)
 print(model.parameters)
+
 criterion = torch.nn.MSELoss()    # mean-squared error for regression
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-#optimizer = torch.optim.SGD(lstm.parameters(), lr=learning_rate)
 
 
 list_losses = []
@@ -58,9 +61,8 @@ for epoch in range(num_epochs):
     number_of_batch = 0
 
     for batch in train_loader:
-        number_of_batch += 1
-        number_of_samples = batch[0].shape[0]
-        #print("Number of samples in this batch : ", number_of_samples)
+
+        model.train()
 
         u_f0, u_loudness, e_f0, e_loudness, e_f0_mean, e_f0_stddev = batch
 
@@ -72,26 +74,56 @@ for epoch in range(num_epochs):
         e_f0_stddev = torch.Tensor(e_f0_stddev.float())
 
 
-        model_input = torch.cat([u_f0, u_loudness], -1)
         ground_truth = torch.cat([e_f0, e_loudness], -1)
-
+        output = model(u_f0.to(device), u_loudness.to(device))
         
-        output = model(model_input.to(device))
         optimizer.zero_grad()
-
-
 
         # obtain the loss function
         loss = criterion(output, ground_truth.to(device))
-        
         loss.backward()
+
+        train_loss = loss.item() / batch[0].size(-1)
         
         optimizer.step()
 
 
-    if epoch % 10 == 0:
-        print("Epoch: %d, loss: %1.5f" % (epoch, loss.item()/number_of_batch))
-        list_losses.append(loss.item()/number_of_batch)
+    # Compute validation loss : 
 
+    model.eval()
+    with torch.no_grad():
+        for batch in test_loader:
+
+            u_f0, u_loudness, e_f0, e_loudness, e_f0_mean, e_f0_stddev = batch
+
+            u_f0 = torch.Tensor(u_f0.float())
+            u_loudness = torch.Tensor(u_loudness.float())
+            e_f0 = torch.Tensor(e_f0.float())
+            e_loudness = torch.Tensor(e_loudness.float())
+            e_f0_mean = torch.Tensor(e_f0_mean.float())
+            e_f0_stddev = torch.Tensor(e_f0_stddev.float())
+
+
+            ground_truth = torch.cat([e_f0, e_loudness], -1)
+            output = model(u_f0.to(device), u_loudness.to(device))
+            
+            loss = criterion(output, ground_truth.to(device))
+            validation_loss = loss.item() / batch[0].size(-1)
+
+
+    
+    if epoch % 10 == 9:
+
+        print("Epoch: %d, training loss: %1.5f" % (epoch+1, train_loss))
+        print("Epoch: %d, validation loss: %1.5f" % (epoch+1, validation_loss))
+
+        writer.add_scalar(f'loss/check_info', {
+            'training': train_loss,
+            'validation': validation_loss,
+            }, epoch+1)
+
+
+
+torch.save(model, 'models/saved_models/LSTM_towards_realistic_midi.pth')
 
 
