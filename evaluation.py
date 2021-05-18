6f0531c1eb048de3df42d8ab2d6c2fde85093d19
georges <g.lebellier@protonmail.com>
2021-05-18 21:32:45 +0200

    Get evaluation results

--- evaluation.py
+++ evaluation.py
@@ -38,7 +38,7 @@ sampling_rate = 100
 _, test_loader = get_datasets(dataset_file = "dataset/contours.csv", sampling_rate = sampling_rate, sample_duration = 20, batch_size = 1, ratio = 0.7, transform=sc.fit_transform)
 test_data = iter(test_loader)
 
-number_of_examples = 2
+number_of_examples = 50
 
 
 
@@ -58,11 +58,9 @@ def get_model_next_step(model, u_f0, u_loudness, e_f0, e_loudness):
 model.eval()
 with torch.no_grad():
     with open("results.npy", "wb") as f:
-        for i in range(10):
-            _ = next(test_data)
-
 
         for i in range(number_of_examples):
+            print("Sample {} reconstruction".format(i))
 
             init_u_f0, init_u_loudness, init_e_f0, init_e_loudness, init_e_f0_mean, init_e_f0_stddev = next(test_data)
             u_f0, u_loudness, e_f0, e_loudness, e_f0_mean, e_f0_stddev = next(test_data)
@@ -83,8 +81,8 @@ with torch.no_grad():
 
 
             n_step = 2000
-            for i in range(n_step):
-                out_f0, out_loudness = get_model_next_step(model, u_f0[:, i:1999+i], u_loudness[:, i:1999+i], out_f0, out_loudness)
+            for j in range(n_step):
+                out_f0, out_loudness = get_model_next_step(model, u_f0[:, j:1999+j].to(device), u_loudness[:, j:1999+j].to(device), out_f0.to(device), out_loudness.to(device))
 
 
             out_f0, out_loudness = out_f0.squeeze().detach(), out_loudness.squeeze().detach()
@@ -94,11 +92,11 @@ with torch.no_grad():
             t = np.array([i/sampling_rate for i in range(out_f0.shape[0])])
             
             np.save(f, t)
-            np.save(f, u_f0)
-            np.save(f, e_f0)
-            np.save(f, out_f0)
-            np.save(f, u_loudness)
-            np.save(f, e_loudness)
-            np.save(f, out_loudness)
+            np.save(f, u_f0.cpu())
+            np.save(f, e_f0.cpu())
+            np.save(f, out_f0.cpu())
+            np.save(f, u_loudness.cpu())
+            np.save(f, e_loudness.cpu())
+            np.save(f, out_loudness.cpu())
             
 
