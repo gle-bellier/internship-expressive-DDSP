04d4c24a72612eeb0a582539ce9471edfb5bc65d
Erik <glebellier@protonmail.com>
2021-06-04 13:02:40 +0200

    Minor adjustment

--- check_dataset.py
+++ check_dataset.py
@@ -2,7 +2,6 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 PATH = "dataset/contours.csv"
 
 u_f0 = []
@@ -22,7 +21,6 @@ with open(PATH) as csvfile:
         e_f0_mean.append(float(row["e_f0_mean"]))
         e_f0_stddev.append(float(row["e_f0_stddev"]))
 
-
 u_f0 = np.array(u_f0)
 u_loudness = np.array(u_loudness)
 e_f0 = np.array(e_f0)
@@ -30,14 +28,13 @@ e_loudness = np.array(e_loudness)
 e_f0_mean = np.array(e_f0_mean)
 e_f0_stddev = np.array(e_f0_stddev)
 
-
 fig, (ax1, ax2) = plt.subplots(2, 1)
-ax1.plot(u_f0.squeeze()[8000:10000], label = "u_f0")
-ax1.plot(e_f0.squeeze()[8000:10000], label = "e_f0")
+ax1.plot(u_f0.squeeze()[8000:16000], label="u_f0")
+ax1.plot(e_f0.squeeze()[8000:16000], label="e_f0")
 ax1.legend()
 
-ax2.plot(u_loudness.squeeze()[8000:10000], label = "u_loudness")
-ax2.plot(e_loudness.squeeze()[8000:10000], label = "e_loudness")
+ax2.plot(u_loudness.squeeze()[8000:16000], label="u_loudness")
+ax2.plot(e_loudness.squeeze()[8000:16000], label="e_loudness")
 ax2.legend()
 
 plt.legend()
