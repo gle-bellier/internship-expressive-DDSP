Fix loudness normalisation

get_contours.py
@@ -117,17 +117,6 @@ class ContoursGetter:
 
         return onsets
 
-    def dB2midi(self, loudness, global_peak=None, global_min=None):
-        loudness = li.core.db_to_amplitude(loudness)
-        if global_peak == None:
-            global_peak = np.max(loudness)
-        if global_min == None:
-            global_min = np.min(loudness)
-
-        l = loudness - global_min
-        L = 127 * np.abs(l) / np.abs(global_peak - global_min)
-        return L
-
     def get_contours(self,
                      dataset_path,
                      midi_file,
@@ -142,15 +131,16 @@ class ContoursGetter:
         ext = Extractor()
         time_wav, frequency_wav, _, loudness_wav = ext.get_time_f0_confidence_loudness(
             dataset_path, wav_file, sampling_rate, block_size, write=True)
-        # loudness mapping :
-        loudness_wav = self.dB2midi(loudness_wav)
+
+        # # loudness mapping : BAD IDEA
+        # loudness_wav = self.dB2midi(loudness_wav)
 
         # From midi file :
         c = Converter()
+
         midi_data = pm.PrettyMIDI(dataset_path + midi_file)
         time_gen, pitch_midi, _ = c.midi2time_f0_loudness(
-            midi_data, times_needed=time_wav
-        )  # sampling_rate/block_size, None)# time_wav)
+            midi_data, times_needed=time_wav)
         frequency_midi = li.core.midi_to_hz(pitch_midi)
 
         midi_onsets = self.get_onsets(dataset_path + midi_file,
@@ -160,11 +150,7 @@ class ContoursGetter:
         frequency_wav_means, frequency_wav_stddev = self.get_freq_mean_stddev(
             frequency_wav, midi_onsets)
 
-        # want to erase really quiet notes
-        #loudness_threshold = 0.20
-        #frequency_gen = frequency_gen * (loudness_gen / np.max(loudness_gen)>loudness_threshold)
-
-        threshold = 20
+        threshold = -5.5
         m = np.array([
             np.mean(self.get_window(loudness_wav, i, sampling_rate // 100))
             for i in range(loudness_wav.shape[0])