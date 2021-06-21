56b16dfcc71a913cfff0a6182573eff5aad4947a
Erik <glebellier@protonmail.com>
2021-06-21 16:21:05 +0200

    refactor code

--- diffusion/utils.py
+++ diffusion/utils.py
@@ -1,5 +1,20 @@
 import torch
 import torch.nn as nn
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class Identity(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return X
+
+    def inverse_transform(self, X, y=None):
+        return X.numpy()
 
 
 class PositionalEncoding(nn.Module):
