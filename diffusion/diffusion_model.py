a32f9fe0b00c918a1304f96da2dc18336ac7d847
Erik <glebellier@protonmail.com>
2021-06-23 17:37:40 +0200

    Add u-net model

--- diffusion/diffusion_model.py
+++ diffusion/diffusion_model.py
@@ -3,120 +3,72 @@ import pytorch_lightning as pl
 from torch import nn
 
 
-class UBlock(nn.Module):
+class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.conv1d = nn.Conv1d(in_channels=in_channels,
-                                out_channels=out_channels,
-                                kernel_size=3,
-                                stride=1,
-                                padding=1)
+        self.conv = nn.Conv1d(in_channels=in_channels,
+                              out_channels=out_channels,
+                              kernel_size=3,
+                              stride=1,
+                              padding=1)
+        self.lr = nn.LeakyReLU()
 
-        self.up_sampling = nn.Upsample(scale_factor=2)
-
-        self.blck = nn.Sequential(
-            nn.Conv1d(in_channels=out_channels,
-                      out_channels=out_channels,
-                      kernel_size=2), nn.LeakyReLU())
-
-    def forward(self, x, ctx):
-
-        print(x.shape)
-        x = self.up_sampling(x)
-        print(x.shape)
-        x = self.conv1d(x)
-        print(x.shape)
-        x = self.blck(x)
-        print(x.shape)
-        x = self.blck(x)
-        print(x.shape)
-
-        return x
+    def forward(self, x):
+        x = self.conv(x)
+        out = self.lr(x)
+        return out
 
 
 class DBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.lr = nn.LeakyReLU()
-        self.conv1d = nn.Conv1d(in_channels=in_channels,
-                                out_channels=out_channels,
-                                kernel_size=3,
-                                stride=1,
-                                padding=1)
-
+        self.conv1 = ConvBlock(in_channels, out_channels)
+        self.conv2 = ConvBlock(out_channels, out_channels)
         self.mp = nn.MaxPool1d(kernel_size=2)
         pass
 
     def forward(self, x):
-        print(x.shape)
-        x = self.conv1d(x)
-        print(x.shape)
-        x = self.lr(x)
-        print(x.shape)
-        x = self.mp(x)
-        print(x.shape)
-
-        return x
+        x = self.conv1(x)
+        x = self.conv2(x)
+        ctx = torch.clone(x)
+        out = self.mp(x)
+        return out, ctx
 
 
-class UNET(pl.LightningModule):
-    def __init__(self, in_size, hidden_size, out_size, scalers):
+class Bottleneck(nn.Module):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.save_hyperparameters()
-        self.scalers = scalers
+        self.conv1 = ConvBlock(in_channels, out_channels)
+        self.conv2 = ConvBlock(out_channels, out_channels)
 
-    def configure_optimizers(self):
-        return torch.optim.Adam(
-            self.parameters(),
-            lr=1e-4,
-            weight_decay=.01,
-        )
 
-    def forward(self, x):
-        x = torch.permute(0, 2, 1)
-        return x
+class UBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv1 = ConvBlock(in_channels, out_channels)
+        self.conv2 = ConvBlock(out_channels, out_channels)
+        self.up_conv = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.ConvTranspose1d(in_channels=in_channels,
+                               out_channels=out_channels,
+                               stride=1,
+                               kernel_size=3,
+                               padding=1))
 
-    def get_losses(self, pred_f0, pred_cents, pred_loudness, target_f0,
-                   target_cents, target_loudness):
-        pass
+        self.lr = nn.LeakyReLU()
 
-    def training_step(self, batch, batch_idx):
-        model_input, target = batch
-        prediction = self.forward(model_input.float())
+    def add_ctx(self, x, ctx):
+        # crop context (y)
+        d_shape = (ctx.shape[-1] - x.shape[-1]) // 2
+        crop = ctx[:, :, d_shape:d_shape + x.shape[2]]
+        #concatenate
+        out = torch.cat([x, crop], 1)
+        return out
 
-        pred_f0, pred_cents, pred_loudness = self.split_predictions(prediction)
-        target_f0, target_cents, target_loudness = torch.split(target, 1, -1)
-
-        loss_f0, loss_cents, loss_loudness = self.get_losses(
-            pred_f0,
-            pred_cents,
-            pred_loudness,
-            target_f0,
-            target_cents,
-            target_loudness,
-        )
-
-        self.log("loss_f0", loss_f0)
-        self.log("loss_cents", loss_cents)
-        self.log("loss_loudness", loss_loudness)
-
-        return loss_f0 + loss_cents + loss_loudness
-
-    @torch.no_grad()
-    def generation_loop(self, x, infer_pitch=False):
-        pass
-
-    def apply_inverse_transform(self, x, idx):
-        scaler = self.scalers[idx]
-        x = x.cpu()
-        out = scaler.inverse_transform(x.reshape(-1, 1))
-        out = torch.from_numpy(out).to("cuda")
-        out = out.unsqueeze(0)
-        return out.float()
-
-    def get_audio(self, model_input, target):
-        pass
-
-    def validation_step(self, batch, batch_idx):
-        model_input, target = batch
-        prediction = self.forward(model_input.float())
+    def forward(self, x, ctx):
+        x = self.up_conv(x)
+        x = self.add_ctx(x, ctx)
+        x = self.conv1(x)
+        out = self.conv2(x)
+        return out
