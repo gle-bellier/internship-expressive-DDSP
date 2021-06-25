e13a3f21e40a0ab4577ca754d5cb5cc13675dba9
Erik <glebellier@protonmail.com>
2021-06-25 08:23:33 +0200

    Major fixes

--- diffusion/toDELETEMODEL.py
+++ diffusion/toDELETEMODEL.py
@@ -16,28 +16,13 @@ class DiffusionModel(pl.LightningModule):
         self.up_channels_in = up_channels[:-1]
         self.up_channels_out = up_channels[1:]
 
-        self.down_blocks = nn.ModuleList([
-            DBlock(in_channels=channels_in, out_channels=channels_out)
-            for channels_in, channels_out in zip(self.down_channels_in,
-                                                 self.down_channels_out)
-        ])
-
-        self.up_blocks = nn.ModuleList([
-            UBlock(in_channels=channels_in, out_channels=channels_out)
-            for channels_in, channels_out in zip(self.up_channels_in,
-                                                 self.up_channels_out)
-        ])
-
-        self.films = nn.ModuleList([
-            FiLM(in_channels=channels_in, out_channels=channels_out)
-            for channels_in, channels_out in zip(self.down_channels_out,
-                                                 self.up_channels_in)
-        ])
-
     def down_sampling(self, x):
         l_out = []
-        for i in range(len(self.down_blocks)):
-            x = self.down_blocks[i](x)
+        for i in range(len(self.down_channels_in) - 1):
+            # print("DownBlock {} : {} -> {}".format(i, self.channels[i],
+            #                                        self.channels[i + 1]))
+            x = DBlock(in_channels=self.down_channels_in[i],
+                       out_channels=self.down_channels_out[i])(x)
             l_out = l_out + [x]
         return l_out
 
@@ -45,24 +30,33 @@ class DiffusionModel(pl.LightningModule):
         l_film_pitch = l_film_pitch[::-1]
         l_film_noisy = l_film_noisy[::-1]
 
-        print(self.up_blocks)
-        for i in range(len(self.up_blocks)):
-            x = self.up_blocks[i](x, l_film_pitch[i], l_film_noisy[i])
+        print(self.up_channels)
+        for i in range(len(self.up_channels) - 1):
+            print("UpBlock {} : {} -> {}".format(i, self.up_channels[i],
+                                                 self.up_channels[i + 1]))
+
+            x = UBlock(in_channels=self.up_channels_in[i],
+                       out_channels=self.up_channels_out[i])(x,
+                                                             l_film_pitch[i],
+                                                             l_film_noisy[i])
+
         return x
 
     def film(self, l_out, noise_level):
         l_film = []
-        for i in range(len(self.films)):
+        for i in range(len(self.down_channels) - 1):
             # print("FiLM {} : {} -> {}".format(i, self.down_channels[i + 1],
             #                                   self.up_channels[i + 1]))
-            f = self.films[i](l_out[i], noise_level)
+            f = FiLM(in_channels=self.down_channels[i + 1],
+                     out_channels=self.up_channels[i + 1])(l_out[i],
+                                                           noise_level)
             l_film = [f] + l_film
         return l_film
 
     def cat_hiddens(self, h_pitch, h_noisy):
         hiddens = torch.cat((h_pitch, h_noisy), dim=-1)
-        out = nn.Conv1d(in_channels=self.down_channels_out[-1],
-                        out_channels=self.up_channels_in[0],
+        out = nn.Conv1d(in_channels=self.down_channels[-1],
+                        out_channels=self.up_channels[0],
                         kernel_size=3,
                         stride=1,
                         padding=1)(hiddens)
