a9e1ac98989d3ca2d67092dff12a18fe24ddf15b
Erik <glebellier@protonmail.com>
2021-06-28 16:58:59 +0200

    Add training methods

--- diffusion/diffusion_model.py
+++ diffusion/diffusion_model.py
@@ -1,13 +1,17 @@
 import torch
 import pytorch_lightning as pl
 from torch import nn
-from utils import FeatureWiseAffine, FiLM, PositionalEncoding, ConvBlock
+from utils import FiLM, Identity
 from downsampling import DBlock
 from upsampling import UBlock
+from diffusion import DiffusionModel
+from torch.utils.data import DataLoader, Dataset, random_split
+from sklearn.preprocessing import StandardScaler, QuantileTransformer, MinMaxScaler
+from diffusion_dataset import DiffusionDataset
 
 
-class UNet_Diffusion(pl.LightningModule):
-    def __init__(self, down_channels, up_channels):
+class UNet_Diffusion(pl.LightningModule, DiffusionModel):
+    def __init__(self, down_channels, up_channels, scalers):
         super().__init__()
         self.save_hyperparameters()
         self.down_channels_in = down_channels[:-1]
@@ -15,6 +19,8 @@ class UNet_Diffusion(pl.LightningModule):
 
         self.up_channels_in = up_channels[:-1]
         self.up_channels_out = up_channels[1:]
+
+        self.scalers = scalers
 
         self.down_blocks_pitch = nn.ModuleList([
             DBlock(in_channels=channels_in, out_channels=channels_out)
@@ -79,7 +85,12 @@ class UNet_Diffusion(pl.LightningModule):
         out = self.cat_conv(hiddens)
         return out
 
-    def forward(self, pitch, noisy, noise_level):
+    def neural_pass(self, x, cdt, noise_level):
+
+        # permute from B, T, C -> B, C, T
+        x = x.permute(0, 2, 1)
+
+        pitch, noisy = torch.split(x, 1, 1)
 
         l_out_pitch = self.down_sampling(self.down_blocks_pitch, pitch)
         l_out_noisy = self.down_sampling(self.down_blocks_noisy, noisy)
@@ -90,18 +101,56 @@ class UNet_Diffusion(pl.LightningModule):
         hiddens = self.cat_hiddens(l_out_pitch[-1], l_out_noisy[-1])
         out = self.up_sampling(hiddens, l_film_pitch, l_film_noisy)
 
+        # permute from B, C, T -> B, T, C
+        out = out.permute(0, 2, 1)
+
         return out
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), 1e-4)
+
+    def training_step(self, batch, batch_idx):
+        loss = self.compute_loss(batch, None)
+        self.log("loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.compute_loss(batch, batch_idx)
+        self.log("val_loss", loss)
+
+    def post_process(self, out):
+        out = out / 2 + .5  # change range
+
+        f0, l0 = out.split(1, 1)
+
+        # Inverse transforms
 
 
 if __name__ == "__main__":
 
-    noisy = torch.randn(1, 2, 64)  # B x C x T
-    pitch = torch.randn(1, 2, 64)
+    trainer = pl.Trainer(
+        gpus=1,
+        callbacks=[pl.callbacks.ModelCheckpoint(monitor="val_total")],
+        max_epochs=10000,
+    )
+    list_transforms = [
+        (MinMaxScaler, ),
+        (QuantileTransformer, 30),
+    ]
 
-    noise_level = torch.tensor(0.3)
+    dataset = DiffusionDataset(list_transforms=list_transforms)
+    val_len = len(dataset) // 20
+    train_len = len(dataset) - val_len
 
-    down_channels = [2, 4, 8, 48]
-    up_channels = [48, 8, 4, 2]
+    train, val = random_split(dataset, [train_len, val_len])
 
-    model = UNet_Diffusion(down_channels, up_channels)
-    out = model(pitch, noisy, noise_level)
+    down_channels = [2, 16, 64, 256]
+    up_channels = [256, 64, 16, 2]
+
+    model = UNet_Diffusion(scalers=dataset.scalers)
+
+    trainer.fit(
+        model,
+        DataLoader(train, 32, True),
+        DataLoader(val, 32),
+    )
