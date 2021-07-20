a2fd51793131dfea3aa894c639d33c92a99d7505
georges <g.lebellier@protonmail.com>
2021-07-20 16:58:23 +0200

    Major fixes (still need tests)

--- unet-rnn/eval.py
+++ unet-rnn/eval.py
@@ -1,52 +1,209 @@
 import torch
-
-torch.set_grad_enabled(False)
-
-#import matplotlib.pyplot as plt
-import soundfile as sf
-from UNet_RNN import UNet_RNN
-
 from pytorch_lightning.callbacks import ModelCheckpoint
-from random import randint
-
 from torch.utils.data import DataLoader, Dataset, random_split
 from sklearn.preprocessing import StandardScaler, QuantileTransformer, MinMaxScaler
-from UNet_dataset import UNet_Dataset
 import numpy as np
 
+torch.set_grad_enabled(False)
+from unet import UNet
+from unet_dataset import UNet_Dataset
+
+from random import randint
 import pickle
+
+
+class DataLoader:
+    def __init__(self,
+                 path="dataset/dataset-unet.pickle",
+                 n_sample=2048,
+                 n_loudness=30,
+                 list_transforms=None):
+
+        with open(path, "rb") as dataset:
+            dataset = pickle.load(dataset)
+
+        self.dataset = dataset
+        self.N = len(dataset["u_f0"])
+        self.n_sample = n_sample
+        self.n_loudness = n_loudness
+        if list_transforms is None:
+            self.list_transforms = [(StandardScaler, ),
+                                    (QuantileTransformer, 30)]
+        else:
+            self.list_transforms = list_transforms
+
+        self.scalers = self.fit_transforms()
+
+    def fit_transforms(self):
+        scalers = []
+
+        # pitch :
+
+        cat = np.concatenate((self.dataset["u_f0"], self.dataset["e_f0"]))
+        contour = cat.reshape(-1, 1)
+        transform = self.list_transforms[0]
+        sc = transform[0]
+        sc = sc(*transform[1:]).fit(contour)
+        scalers.append(sc)
+
+        # loudness
+
+        contour = self.dataset["e_loudness"]
+        contour = contour.reshape(-1, 1)
+        transform = self.list_transforms[1]
+        sc = transform[0]
+        sc = sc(*transform[1:]).fit(contour)
+        scalers.append(sc)
+
+        return scalers
+
+    def apply_transform(self, x, scaler):
+        out = scaler.transform(x.reshape(-1, 1)).squeeze(-1)
+        return out
+
+    def inverse_tranform(self, out):
+
+        f0, l0 = torch.split(out, 1, -1)
+
+        f0 = f0.reshape(-1, 1).cpu().numpy()
+        l0 = l0.reshape(-1, 1).cpu().numpy()
+
+        # Inverse transforms
+        f0 = self.scalers[0].inverse_transform(f0).reshape(-1)
+        l0 = self.scalers[1].inverse_transform(l0).reshape(-1)
+
+        return f0, l0
+
+    def get_quantized_loudness(self, e_l0, events):
+        #compute sum of all events :
+        e = torch.abs(onsets + offsets)
+        e = torch.tensor(
+            [e[i] if e[i + 1] != 1 else 0 for i in range(len(e) - 1)])
+        u_l0 = torch.zeros_like(e_l0)
+        # get indexes of events
+        indexes = (e == 1).nonzero(as_tuple=True)[0]
+        start, end = torch.tensor([0]), torch.tensor([e_l0.shape[0] - 1])
+        indexes = torch.cat([start, indexes, end], -1)
+
+        for i in range(len(indexes) - 1):
+            u_l0[indexes[i]:indexes[i + 1]] = torch.mean(
+                e_l0[indexes[i]:indexes[i + 1]])
+        return u_l0
+
+    def __len__(self):
+        return self.N // self.n_sample
+
+    def __getitem__(self, idx):
+        N = self.n_sample
+        idx *= N
+
+        jitter = randint(0, N // 10)
+        idx += jitter
+        idx = max(idx, 0)
+        idx = min(idx, len(self) * self.n_sample - self.n_sample)
+
+        u_f0 = self.dataset["u_f0"][idx:idx + self.n_sample]
+        e_f0 = self.dataset["e_f0"][idx:idx + self.n_sample]
+        e_l0 = self.dataset["e_loudness"][idx:idx + self.n_sample]
+        onsets = self.dataset["onsets"][idx:idx + self.n_sample]
+        offsets = self.dataset["offsets"][idx:idx + self.n_sample]
+
+        # Apply transforms :
+
+        u_f0 = self.apply_transform(u_f0, self.scalers[0])
+        e_f0 = self.apply_transform(e_f0, self.scalers[0])
+        e_l0 = self.apply_transform(e_l0, self.scalers[1])
+
+        u_f0 = torch.from_numpy(u_f0).float()
+        e_f0 = torch.from_numpy(e_f0).float()
+        e_l0 = torch.from_numpy(e_l0).float()
+        onsets = torch.from_numpy(onsets).float()
+        offsets = torch.from_numpy(offsets).float()
+
+        u_l0 = self.get_quantized_loudness(e_l0, onsets, offsets)
+
+        model_input = torch.cat([
+            u_f0.unsqueeze(-1),
+            u_l0.unsqueeze(-1),
+        ], -1)
+
+        target = torch.cat([
+            e_f0.unsqueeze(-1),
+            e_l0.unsqueeze(-1),
+        ], -1)
+
+        return model_input, target, onsets, offsets
+
 
 list_transforms = [
     (MinMaxScaler, ),
     (QuantileTransformer, 30),
 ]
 
-dataset = UNet_Dataset(list_transforms=list_transforms)
-val_len = len(dataset) // 20
-train_len = len(dataset) - val_len
-train, val = random_split(dataset, [train_len, val_len])
+PATH = "dataset/dataset-diffusion.pickle"
+dataset = DataLoader(PATH, list_transforms=list_transforms)
 
 down_channels = [2, 16, 512, 1024]
 ddsp = torch.jit.load("ddsp_debug_pretrained.ts").eval()
 
-model = UNet_RNN.load_from_checkpoint(
-    "unet_rnn/lightning_logs/version_7/checkpoints/epoch=9518-step=142784.ckpt",
+model = UNet.load_from_checkpoint(
+    "logs/unet/default/version_1/checkpoints/epoch=114-step=1694.ckpt",
     scalers=dataset.scalers,
     channels=down_channels,
     ddsp=ddsp,
     strict=False).eval()
 
-NB_EXAMPLES = 5
+#model.ddsp = torch.jit.load("ddsp_debug_pretrained.ts").eval()
 
-for i in range(NB_EXAMPLES):
-    model_input, target = dataset[randint(0, len(dataset))]
+# Initialize data :
 
-    model_input = model_input.unsqueeze(0)
-    pred = model(model_input)
-    f0, lo = model.post_process(pred)
+u_f0 = np.empty(0)
+u_lo = np.empty(0)
+e_f0 = np.empty(0)
+e_lo = np.empty(0)
+pred_f0 = np.empty(0)
+pred_lo = np.empty(0)
+onsets = np.empty(0)
+offsets = np.empty(0)
 
-    f0 = torch.from_numpy(f0).float().reshape(1, -1, 1)
-    lo = torch.from_numpy(lo).float().reshape(1, -1, 1)
+# Prediction loops :
 
-    audio = ddsp(f0, lo).reshape(-1).numpy()
-    sf.write("results/unet-rnn/samples/sample{}.wav".format(i), audio, 16000)
+N_EXAMPLE = 5
+for i in range(N_EXAMPLE):
+    midi, target, ons, offs = dataset.get()
+
+    n_step = 10
+    out = model(midi.unsqueeze(0), midi.unsqueeze(0))
+
+    f0, lo = dataset.inverse_transform(out)
+    midi_f0, midi_lo = dataset.inverse_transform(midi)
+    target_f0, target_lo = dataset.inverse_transform(target)
+
+    # add to results:
+
+    f0, lo = out.split(1, -1)
+    midi_f0, midi_lo = midi.split(1, -1)
+    target_f0, target_lo = target.split(1, -1)
+
+    u_f0 = np.concatenate((u_f0, midi_f0.squeeze()))
+    u_lo = np.concatenate((u_lo, midi_lo.squeeze()))
+
+    e_f0 = np.concatenate((e_f0, target_f0.squeeze()))
+    e_lo = np.concatenate((e_lo, target_lo.squeeze()))
+
+    pred_f0 = np.concatenate((pred_f0, f0.squeeze()))
+    pred_lo = np.concatenate((pred_lo, lo.squeeze()))
+
+out = {
+    "u_f0": u_f0,
+    "u_lo": u_lo,
+    "e_f0": e_f0,
+    "e_lo": e_lo,
+    "pred_f0": pred_f0,
+    "pred_lo": pred_lo,
+    "onsets": onsets,
+    "offsets": offsets
+}
+
+with open("results/unet-rnn/data/results-raw.pickle", "wb") as file_out:
+    pickle.dump(out, file_out)
