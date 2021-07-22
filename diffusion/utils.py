9029b656c44998df1161558802ae689f49b05bb9
Erik <glebellier@protonmail.com>
2021-07-22 15:28:59 +0200

    Add new transform

--- diffusion/utils.py
+++ diffusion/utils.py
@@ -1,20 +1,5 @@
 import torch
 import torch.nn as nn
-from sklearn.base import BaseEstimator, TransformerMixin
-
-
-class Identity(BaseEstimator, TransformerMixin):
-    def __init__(self):
-        pass
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
-        return X
-
-    def inverse_transform(self, X, y=None):
-        return X.numpy()
 
 
 class ConvBlock(nn.Module):
