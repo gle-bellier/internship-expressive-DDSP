acd4e7e73db1d7f29b73ac369fdbfb3186e98386
Antoine <caillon@ircam.fr>
2021-08-09 18:02:39 +0200

    add resume mechanism

--- train_redif.py
+++ train_redif.py
@@ -4,12 +4,24 @@ from redif.ema import EMAModelCheckPoint
 import torch
 from torch.utils.data import DataLoader, random_split
 import pytorch_lightning as pl
+from effortless_config import Config
+
+
+class args(Config):
+    CKPT = None
+
+
+args.parse_args()
 
 dataset = Dataset(256)
 val_n = len(dataset) // 50
 train, val = random_split(dataset, [len(dataset) - val_n, val_n])
 
-model = Model(2, 128, [128, 192, 256, 384, 512])
+if args.CKPT is not None:
+    model = Model.load_from_checkpoint(args.CKPT, strict=False)
+else:
+    model = Model(2, 128, [128, 192, 256, 384, 512])
+
 model.set_noise_schedule()
 model.transform.compute_stats(dataset.e_f0, dataset.e_lo)
 model.ddsp = torch.jit.load("ddsp_violin_pretrained.ts")
@@ -19,7 +31,9 @@ trainer = pl.Trainer(gpus=1,
                      callbacks=[EMAModelCheckPoint(
                          model,
                          filename="ema",
-                     )])
+                     )],
+                     max_epochs=100000,
+                     resume_from_checkpoint=args.CKPT)
 trainer.fit(
     model,
     DataLoader(train, 16, True, drop_last=True),
