d193605d0b18fe3db981b73bbc8aa472487bde5a
Antoine <caillon@ircam.fr>
2021-08-09 15:25:33 +0200

    RTT

--- train_redif.py
+++ train_redif.py
@@ -11,6 +11,14 @@ train, val = random_split(dataset, [len(dataset) - val_n, val_n])
 model = Model(2, 128, [128, 256, 384, 512])
 model.set_noise_schedule()
 model.transform.compute_stats(dataset.e_f0, dataset.e_lo)
+model.ddsp = torch.jit.load("ddsp_violin_pretrained.ts")
 
-trainer = pl.Trainer(gpus=0)
-trainer.fit(model, train, val)
+trainer = pl.Trainer(
+    gpus=1,
+    check_val_every_n_epoch=10,
+)
+trainer.fit(
+    model,
+    DataLoader(train, 16, True, drop_last=True),
+    DataLoader(val, 16, False),
+)
