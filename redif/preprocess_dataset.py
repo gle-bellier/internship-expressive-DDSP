857646d05fee6f045d69d93ae6abf698ec2a470a
Antoine <caillon@ircam.fr>
2021-08-11 15:22:05 +0200

    preprocess csv directly

--- redif/preprocess_dataset.py
+++ redif/preprocess_dataset.py
@@ -1,33 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pickle
 
 
 def ftom(f):
     return 12 * (np.log(f) - np.log(440)) / np.log(2) + 69
 
 
-instrument = "violin"
-path = "dataset/{}-train-da.pickle".format(instrument)
-print("dataset file used : {}".format(path))
-print("Loading Dataset...")
-with open(path, "rb") as dataset:
-    dataset = pickle.load(dataset)
+with open("dataset/contours.csv", "r") as contours:
+    contours = contours.read()
 
-u_f0 = np.asarray(dataset["u_f0"])
-u_lo = np.asarray(dataset["u_loudness"])
-e_f0 = np.asarray(dataset["e_f0"])
-e_lo = np.asarray(dataset["e_loudness"])
+contours = contours.split("\n")
+contours.pop(0)
+contours.pop(-1)
 
-u_f0 = np.round(ftom(u_f0)).astype(int).clip(0, 127)
-e_f0 = np.clip(e_f0, 1e-4, None)
+data = {
+    "u_f0": [],
+    "u_lo": [],
+    "e_f0": [],
+    "e_lo": [],
+}
 
-plt.plot(u_f0[:5000])
-plt.plot(e_f0[:5000])
-plt.show()
+for t in contours:
+    u_f0, u_loudness, e_f0, e_loudness, _, _, _, _ = t.split(",")
+    data["u_f0"].append(float(u_f0))
+    data["u_lo"].append(float(u_loudness))
+    data["e_f0"].append(float(e_f0))
+    data["e_lo"].append(float(e_loudness))
+
+u_f0 = np.asarray(data["u_f0"])
+u_lo = np.asarray(data["u_lo"])
+e_f0 = np.asarray(data["e_f0"])
+e_lo = np.asarray(data["e_lo"])
+
+u_f0 = np.round(ftom(u_f0)).astype(int)
 
 assert all(u_f0 >= 0) and all(u_f0 < 128)
-assert all(e_f0 > 0)
 
 np.save("u_f0.npy", u_f0)
 np.save("u_lo.npy", u_lo)
