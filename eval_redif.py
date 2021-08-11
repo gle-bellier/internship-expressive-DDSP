44666cc0bfdcbd6a75030d71ff56b3184d15c781
georges <g.lebellier@protonmail.com>
2021-08-11 19:14:22 +0200

    Update eval redif to midi contours

--- eval_redif.py
+++ eval_redif.py
@@ -21,6 +21,8 @@ args.parse_args()
 
 makedirs(args.OUT, exist_ok=True)
 n_sample = len(glob(path.join(args.OUT, "*.wav")))
+sample_len = 500
+nb_samples = 10
 
 # INSTANCIATE MODELS
 model = Model.load_from_checkpoint(args.CKPT, strict=False).eval()
@@ -29,24 +31,35 @@ ddsp = torch.jit.load(args.DDSP).eval()
 
 ################################################
 # TODO: replace dummy input with actual contours
-pitch = torch.from_numpy(np.load("u_f0.npy")[:1024]).reshape(1, -1).long()
-loudness = torch.from_numpy(np.load("u_lo.npy")[:1024]).reshape(1, -1).float()
+
+patch = "redifcontours/"
+name = "flute-test"
+
+pitch_all = torch.from_numpy(np.load("{}u_f0{}.npy".format(path,
+                                                           name))).reshape(
+                                                               1, -1).long()
+loudness_all = torch.from_numpy(np.load("{}u_lo{}.npy".format(
+    path, name))).reshape(1, -1).float()
 ################################################
 
-# FORMAT INPUT CONTOURS
-pitch = nn.functional.one_hot(pitch, 127).permute(0, 2, 1)
-loudness = loudness.unsqueeze(1)
+for i in range(nb_samples):
 
-assert torch.all(pitch >= 0) and torch.all(pitch < 128), str(pitch)
+    pitch = pitch_all[sample_len * i:sample_len * (i + 1)]
+    loudness = loudness_all[sample_len * i:sample_len * (i + 1)]
+    # FORMAT INPUT CONTOURS
+    pitch = nn.functional.one_hot(pitch, 127).permute(0, 2, 1)
+    loudness = loudness.unsqueeze(1)
 
-env = torch.cat([pitch, loudness], 1)
-x = torch.randn(env.shape[0], model.data_dim, env.shape[-1])
+    assert torch.all(pitch >= 0) and torch.all(pitch < 128), str(pitch)
 
-# SAMPLE FROM ESTIMATED DISTRIBUTION
-y = model.sample(x, env)
-f0, lo = model.transform.inverse(y)
+    env = torch.cat([pitch, loudness], 1)
+    x = torch.randn(env.shape[0], model.data_dim, env.shape[-1])
 
-# SYNTHESIS
-sound = ddsp(f0.permute(0, 2, 1), lo.permute(0, 2, 1))
-name = f"sample_{n_sample:03d}.wav"
-sf.write(path.join(args.OUT, name), sound.reshape(-1).numpy(), 16000)
+    # SAMPLE FROM ESTIMATED DISTRIBUTION
+    y = model.sample(x, env)
+    f0, lo = model.transform.inverse(y)
+
+    # SYNTHESIS
+    sound = ddsp(f0.permute(0, 2, 1), lo.permute(0, 2, 1))
+    name = f"{name}_{i + n_sample:03d}.wav"
+    sf.write(path.join(args.OUT, name), sound.reshape(-1).numpy(), 16000)
