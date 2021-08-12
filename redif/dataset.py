3752e5a5f8f0057aef78e19ad82cdb77f2a38bb0
Antoine <caillon@ircam.fr>
2021-08-12 11:36:02 +0200

    fix dataset

--- redif/dataset.py
+++ redif/dataset.py
@@ -3,24 +3,24 @@ import numpy as np
 from os import path
 
 
-def load(path, name, dtype=torch.long):
+def load(data_path, name, dtype=torch.long):
     """
-    loads a numpy array located in path/name and convert it
+    loads a numpy array located in data_path/name and convert it
     to a torch.Tensor with dtype 
     """
-    array = np.load(path.join(path, name))
+    array = np.load(path.join(data_path, name))
     tensor = torch.from_numpy(array)
-    tensor = tensor.to(torch.long)
+    tensor = tensor.to(dtype)
     return tensor
 
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, n_step, data_path):
         super().__init__()
-        self.u_f0 = load(data_path, "u_f0", torch.long)
-        self.u_lo = load(data_path, "u_lo", torch.float)
-        self.e_f0 = load(data_path, "e_f0", torch.float)
-        self.e_lo = load(data_path, "e_lo", torch.float)
+        self.u_f0 = load(data_path, "u_f0.npy", torch.long)
+        self.u_lo = load(data_path, "u_lo.npy", torch.float)
+        self.e_f0 = load(data_path, "e_f0.npy", torch.float)
+        self.e_lo = load(data_path, "e_lo.npy", torch.float)
 
         self.n_step = n_step
 
